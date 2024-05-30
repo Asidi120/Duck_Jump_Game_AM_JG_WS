c424ab42c8cc35b79366155f692979aec95ab2e8
Agnieszka <am310943@student.polsl.pl>
2024-05-30 16:01:09 +0200

    Okienko

--- Duck_Jump_Game_/Duck_Jump_Game_.cpp
+++ Duck_Jump_Game_/Duck_Jump_Game_.cpp
@@ -1,17 +1,18 @@
 ﻿#include "SFML/Graphics.hpp" // w tym miejscu nawiasy ostre (youtube uniemożliwia umieszczenie ich w opisie filmu)
 
 int main(void) {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(700, 900), "Duck_Jump");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen()) 
+    while (window.isOpen()) //:DD
     {
         sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) 
+        while (window.pollEvent(event)) //ko
+        {
+            if (event.type == sf::Event::Closed)
                 window.close();
-        }
+        }//dkd
 
         window.clear();
         window.draw(shape);
