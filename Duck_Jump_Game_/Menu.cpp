6ffc88848a00b15f33967367686b1933a6345669
Jakub Grabowski <jg310760@student.polsl.pl>
2024-05-31 19:48:16 +0200

    tło

--- Duck_Jump_Game_/Menu.cpp
+++ Duck_Jump_Game_/Menu.cpp
@@ -3,11 +3,11 @@
 
 Menu::Menu(): wybrany_obiekt(0)
 {
-	if (!czcionka.loadFromFile("Asul-Regular.ttf"))
+	if (!czcionka.loadFromFile("GloriaHallelujah-Regular.ttf"))
 	{
 		cout << "Blad czcionki" << endl;
 	}
-	if (!tekstura_tla.loadFromFile("rybka.png", IntRect(0, 0, 1000, 1000)))
+	if (!tekstura_tla.loadFromFile("chmurki2.png", IntRect(0, 0, 1000, 1000)))
 	{
 		cout << "Blad ladowania tla" << endl;
 	}
@@ -20,23 +20,23 @@ Menu::Menu(): wybrany_obiekt(0)
 	tytul.setPosition(Vector2f(180,150));
 
 	menu[0].setFont(czcionka);
-	menu[0].setFillColor(Color::Cyan);
+	menu[0].setFillColor(Color::Red);
 	menu[0].setString("Graj");
-	menu[0].setCharacterSize(28);
+	menu[0].setCharacterSize(38);
 	menu[0].setPosition(Vector2f(300,350));
 
 	menu[1].setFont(czcionka);
-	menu[1].setFillColor(Color::White);
+	menu[1].setFillColor(Color::Black);
 	menu[1].setString("Postacie");
 	menu[1].setPosition(Vector2f(270,450));
 
 	menu[2].setFont(czcionka);
-	menu[2].setFillColor(Color::White);
+	menu[2].setFillColor(Color::Black);
 	menu[2].setString("Ustawienia");
 	menu[2].setPosition(Vector2f(255,550));
 
 	menu[3].setFont(czcionka);
-	menu[3].setFillColor(Color::White);
+	menu[3].setFillColor(Color::Black);
 	menu[3].setString("Wyjdz");
 	menu[3].setPosition(Vector2f(290,650));
 }
@@ -55,14 +55,14 @@ void Menu::ruch_do_gory(int ilosc_linijek)
 {
 	if (wybrany_obiekt - 1 >= 0)
 	{
-		menu[wybrany_obiekt].setFillColor(Color::White);
+		menu[wybrany_obiekt].setFillColor(Color::Black);
 		wybrany_obiekt--;
-		menu[wybrany_obiekt].setFillColor(Color::Cyan);
+		menu[wybrany_obiekt].setFillColor(Color::Red);
 	}
 	else
 	{
-		menu[ilosc_linijek-1].setFillColor(Color::Cyan);
-		menu[wybrany_obiekt].setFillColor(Color::White);
+		menu[ilosc_linijek-1].setFillColor(Color::Red);
+		menu[wybrany_obiekt].setFillColor(Color::Black);
 		wybrany_obiekt= ilosc_linijek - 1;
 	}
 }
@@ -70,14 +70,14 @@ void Menu::ruch_w_dol(int ilosc_linijek)
 {
 	if (wybrany_obiekt + 1 <= (ilosc_linijek-1))
 	{
-		menu[wybrany_obiekt].setFillColor(Color::White);
+		menu[wybrany_obiekt].setFillColor(Color::Black);
 		wybrany_obiekt++;
-		menu[wybrany_obiekt].setFillColor(Color::Cyan);
+		menu[wybrany_obiekt].setFillColor(Color::Red);
 	}
 	else
 	{
-		menu[0].setFillColor(Color::Cyan);
-		menu[wybrany_obiekt].setFillColor(Color::White);
+		menu[0].setFillColor(Color::Red);
+		menu[wybrany_obiekt].setFillColor(Color::Black);
 		wybrany_obiekt = 0;
 	}
 }
