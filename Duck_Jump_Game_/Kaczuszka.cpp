1e7b2c4929fc3ae0b354a39a443376bc0e273dca
Agnieszka <am310943@student.polsl.pl>
2024-06-02 15:59:52 +0200

    oh kubus

--- Duck_Jump_Game_/Kaczuszka.cpp
+++ Duck_Jump_Game_/Kaczuszka.cpp
@@ -18,18 +18,22 @@ void Kaczuszka::rysuj_gracza(RenderWindow& okno)
 
 void Kaczuszka::ruch(float kierunek_x, float kierunek_y)
 {
-	float przemieszczenie_x = 10.f;
+	float przemieszczenie_x = 12.f;
 	if (kierunek_x == 1 && kierunek_y == 0)
 	{
-		if (kaczuszka.getPosition().x >= 650)
+		if (kaczuszka.getPosition().x >= 610)
 		{
-			kaczuszka.setPosition(Vector2f((0.f-kaczuszka.getSize().x), kaczuszka.getPosition().y));
+			kaczuszka.setPosition(Vector2f((0.f-(kaczuszka.getSize().x)), kaczuszka.getPosition().y));
+			kaczuszka.move(przemieszczenie_x, 0.f);
 		}
-		kaczuszka.move(przemieszczenie_x, 0.f);
+		else
+		{
+			kaczuszka.move(przemieszczenie_x, 0.f);
+		}
 	}
 	if (kierunek_x == -1 && kierunek_y == 0)
 	{
-		if (kaczuszka.getPosition().x <= (0-kaczuszka.getSize().x))
+		if (kaczuszka.getPosition().x <= (0-kaczuszka.getSize().x)+40)
 		{
 			kaczuszka.setPosition(Vector2f(650.f, kaczuszka.getPosition().y));
 		}
