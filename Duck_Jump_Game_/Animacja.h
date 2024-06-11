a73fe466a5c08de51e37999eb392d1d744c688a5
Agnieszka <am310943@student.polsl.pl>
2024-06-11 19:47:17 +0200

    .

--- Duck_Jump_Game_/Animacja.h
+++ Duck_Jump_Game_/Animacja.h
@@ -5,7 +5,7 @@ class Kaczuszka;
 class Animacja
 {
 public:
-	const int ilosc_obrazkow=3, wysokosc_obrazka=24, szerokosc_obrazka=20;
+	const int ilosc_obrazkow=3, wysokosc_obrazka=23, szerokosc_obrazka=17;
 	int i{};
 	int aktualna_klatka{};
 	Clock czas_zmiany;
