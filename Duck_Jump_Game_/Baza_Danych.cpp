77572b34bdcce6b0cb5c4aabf560a92c028a37e8
Agnieszka <am310943@student.polsl.pl>
2024-06-14 18:30:56 +0200

    .

--- Duck_Jump_Game_/Baza_Danych.cpp
+++ Duck_Jump_Game_/Baza_Danych.cpp
@@ -1,105 +1,121 @@
 #include "Baza_Danych.h"
+
+
 // Funkcja tworz¹ca bazê danych, jeœli jeszcze nie istnieje
-void Baza_Danych::stworzBaze() {
+void Baza_Danych::stworzBaze() 
+{
     sqlite3* baza;
     int exit = sqlite3_open(sciezka_do_bazy, &baza);
     sqlite3_close(baza);
 }
 
 // Funkcja tworz¹ca tabelê GRACZE, jeœli jeszcze nie istnieje
-void Baza_Danych::stworzTabele() {
+void Baza_Danych::stworzTabele() 
+{
     sqlite3* baza;
-    char* bladWiadomosc;
+    char* wiadomosc_bledu;
 
     string sql = "CREATE TABLE IF NOT EXISTS GRACZE("
         "ID_GRACZA INTEGER PRIMARY KEY AUTOINCREMENT, "
         "NAZWA_GRACZA TEXT NOT NULL, "
-        "NAJLEPSZY_WYNIK INT NOT NULL, "
-        "CHLEBKI INT NOT NULL);";
+        "NAJLEPSZY_WYNIK INT, "
+        "CHLEBKI INT);";
 
     int exit = sqlite3_open(sciezka_do_bazy, &baza);
-    exit = sqlite3_exec(baza, sql.c_str(), NULL, 0, &bladWiadomosc);
-    if (exit != SQLITE_OK) {
+    exit = sqlite3_exec(baza, sql.c_str(), NULL, 0, &wiadomosc_bledu);
+    if (exit!=SQLITE_OK) 
+    {
         cerr << "B³¹d w funkcji stworzTabele." << endl;
-        sqlite3_free(bladWiadomosc);
+        sqlite3_free(wiadomosc_bledu);
     }
-    else {
+    else 
+    {
         cout << "Tabela utworzona pomyœlnie." << endl;
     }
     sqlite3_close(baza);
 }
 
 // Funkcja wstawiaj¹ca dane do tabeli GRACZE
-void Baza_Danych::wstawDane(const string& nazwa_gracza, int najlepszy_wynik, int chlebki) {
+void Baza_Danych::wstawDane(const string& nazwa_gracza, int najlepszy_wynik, int chlebki) 
+{
     sqlite3* baza;
-    char* bladWiadomosc;
+    char* wiadomosc_bledu;
 
     string sql = "INSERT INTO GRACZE (NAZWA_GRACZA, NAJLEPSZY_WYNIK, CHLEBKI) VALUES('"
         + nazwa_gracza + "', " + to_string(najlepszy_wynik) + ", " + to_string(chlebki) + ");";
 
     int exit = sqlite3_open(sciezka_do_bazy, &baza);
-    exit = sqlite3_exec(baza, sql.c_str(), NULL, 0, &bladWiadomosc);
-    if (exit != SQLITE_OK) {
+    exit = sqlite3_exec(baza, sql.c_str(), NULL, 0, &wiadomosc_bledu);
+    if (exit != SQLITE_OK) 
+    {
         cerr << "B³¹d w funkcji wstawDane." << endl;
-        sqlite3_free(bladWiadomosc);
+        sqlite3_free(wiadomosc_bledu);
     }
-    else {
+    else 
+    {
         cout << "Rekordy wstawione pomyœlnie!" << endl;
     }
     sqlite3_close(baza);
 }
 
 // Funkcja aktualizuj¹ca dane w tabeli GRACZE
-void Baza_Danych::aktualizujDane(int id_gracza, int najlepszy_wynik, int chlebki) {
+void Baza_Danych::aktualizujDane(int id_gracza, int najlepszy_wynik, int chlebki) 
+{
     sqlite3* baza;
-    char* bladWiadomosc;
+    char* wiadomosc_bledu;
 
     string sql = "UPDATE GRACZE SET NAJLEPSZY_WYNIK = " + to_string(najlepszy_wynik) +
         ", CHLEBKI = " + to_string(chlebki) + " WHERE ID_GRACZA = " + to_string(id_gracza) + ";";
 
     int exit = sqlite3_open(sciezka_do_bazy, &baza);
-    exit = sqlite3_exec(baza, sql.c_str(), NULL, 0, &bladWiadomosc);
-    if (exit != SQLITE_OK) {
+    exit = sqlite3_exec(baza, sql.c_str(), NULL, 0, &wiadomosc_bledu);
+    if (exit != SQLITE_OK) 
+    {
         cerr << "B³¹d w funkcji aktualizujDane." << endl;
-        sqlite3_free(bladWiadomosc);
+        sqlite3_free(wiadomosc_bledu);
     }
-    else {
+    else 
+    {
         cout << "Rekordy zaktualizowane pomyœlnie!" << endl;
     }
     sqlite3_close(baza);
 }
 
 // Funkcja usuwaj¹ca dane z tabeli GRACZE
-void Baza_Danych::usunDane(int id_gracza) {
+void Baza_Danych::usunDane(int id_gracza) 
+{
     sqlite3* baza;
-    char* bladWiadomosc;
+    char* wiadomosc_bledu;
 
     string sql = "DELETE FROM GRACZE WHERE ID_GRACZA = " + to_string(id_gracza) + ";";
 
     int exit = sqlite3_open(sciezka_do_bazy, &baza);
-    exit = sqlite3_exec(baza, sql.c_str(), callback, NULL, &bladWiadomosc);
-    if (exit != SQLITE_OK) {
+    exit = sqlite3_exec(baza, sql.c_str(), callback, NULL, &wiadomosc_bledu);
+    if (exit != SQLITE_OK) 
+    {
         cerr << "B³¹d w funkcji usunDane." << endl;
-        sqlite3_free(bladWiadomosc);
+        sqlite3_free(wiadomosc_bledu);
     }
-    else {
+    else 
+    {
         cout << "Rekordy usuniête pomyœlnie!" << endl;
     }
     sqlite3_close(baza);
 }
 
 // Funkcja wybieraj¹ca dane z tabeli GRACZE
-void Baza_Danych::wybierzDane() {
+void Baza_Danych::wybierzDane() 
+{
     sqlite3* baza;
-    char* bladWiadomosc;
+    char* wiadomosc_bledu;
 
     string sql = "SELECT * FROM GRACZE;";
 
     int exit = sqlite3_open(sciezka_do_bazy, &baza);
-    exit = sqlite3_exec(baza, sql.c_str(), callback, NULL, &bladWiadomosc);
+    exit = sqlite3_exec(baza, sql.c_str(), callback, NULL, &wiadomosc_bledu);
     if (exit != SQLITE_OK) {
         cerr << "B³¹d w funkcji wybierzDane." << endl;
-        sqlite3_free(bladWiadomosc);
+        sqlite3_free(wiadomosc_bledu);
     }
     else {
         cout << "Rekordy wybrane pomyœlnie!" << endl;
@@ -108,8 +124,10 @@ void Baza_Danych::wybierzDane() {
 }
 
 // Funkcja zwrotna u¿ywana przez sqlite3_exec do przetwarzania wyników zapytañ
-int Baza_Danych::callback(void* NotUsed, int argc, char** argv, char** azColName) {
-    for (int i = 0; i < argc; i++) {
+int Baza_Danych::callback(void* baza, int argc, char** argv, char** azColName) 
+{
+    for (int i = 0; i < argc; i++) 
+    {
         cout << azColName[i] << ": " << (argv[i] ? argv[i] : "NULL") << endl;
     }
     cout << endl;
